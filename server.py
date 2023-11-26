5bb2d82e1c9c3d3c952b0e15bebadc01d3667df8
RAJU <gangarajbopparam@gmail.com>
2023-11-26 06:12:44 +0530

    sdfsfd

---
+++ server.py
@@ -0,0 +1,43 @@
+# Team T06 - IT496 CP3 
+# This is the main file that is used to run the application
+
+# importing libraries
+from flask import Flask,render_template, request, jsonify
+import json
+from predictor import getPrediction
+
+# Initialize app using Flask
+app = Flask(__name__)
+
+jsonDataFile = open('./Dataset/ICC23Data.json')
+jsonData = json.load(jsonDataFile)
+
+# IP Address and Port Number to run the app
+IpAddr = '127.0.0.1'
+Port = 8000
+
+
+# default route
+@app.route('/')
+def index():
+    teamsList = jsonData['TeamsList']
+    venueList = jsonData['VenueList']
+    return render_template("index.html",teamsList = teamsList, venueList=venueList)
+
+# getting the prediction
+@app.route('/predict',methods=['Post'])
+def predict():
+    inputData = request.json
+    team1 = inputData['team1']
+    team2 = inputData['team2']
+    venue = inputData['venue']
+    print(team1,team2,venue)
+    result = getPrediction(team1, team2, venue)
+    return jsonify(result.__dict__)
+
+# Run the API
+if __name__ == '__main__':
+   app.run(host=IpAddr, port=Port,debug=True)
+
+# To run the server, enter the following command in terminal  
+# python server.py
---
+++ static/client.js
@@ -0,0 +1,121 @@
+// Javascript code that runs on the client side
+// To send the request to backend and update the view accordingly
+team1 = null
+team2 = null
+venue = null
+loadingView = document.getElementById('loadingView')
+resultView = document.getElementById('resultView')
+errorView = document.getElementById('errorView')
+
+function showErrorMessage(message) {
+  errorView.innerHTML = message
+  errorView.style.display = 'block'
+}
+
+document.getElementById('team1Select').addEventListener('change', (event) => {
+  team1 = event.target.value
+  if (team1 == team2 && team1 != 0 && team1 != null) {
+    showErrorMessage('Team 1 and Team 2 should not be same!!')
+  }
+})
+
+document.getElementById('team2Select').addEventListener('change', (event) => {
+  team2 = event.target.value
+  if (team1 == team2 && team2 != 0 && team2 != null) {
+    showErrorMessage('Team 1 and Team 2 should not be same!!')
+  }
+})
+
+document.getElementById('venueSelect').addEventListener('change', (event) => {
+  venue = event.target.value
+})
+
+async function sendRequest() {
+
+  // hide previou results and show loading
+  // loadingView.style.display = 'block'
+  resultView.style.display = 'none'
+  loadingView.style.display = 'block'
+  errorView.style.display = 'none'
+
+  // verify the inputs
+  if ((team1 == "0" || team1 == null) || (team2 == "0" || team2 == null) || (venue == "0" || venue == null)) {
+    loadingView.style.display = 'none'
+    showErrorMessage("Please select all the inputs!!")
+    return
+  }
+
+  if (team1 == team2) {
+    loadingView.style.display = 'none'
+    showErrorMessage("Team 1 and Team 2 should not be same!!")
+    return
+  }
+
+  // send the request and get the response
+  const host = window.location.protocol + '//' + window.location.host;
+  const response = await fetch(`${host}/predict`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ team1, team2, venue })
+  })
+
+  let json = await response.json()
+
+  // update the view accordingly
+  document.getElementById('winnerTeam').innerHTML = json['winner']
+  document.getElementById('winnerTeamSub').innerHTML = json['winner']
+  document.getElementById('winnerProbability').innerHTML = json['winningProbability']
+  team1List = json['team1']
+  team2List = json['team2']
+  for (i = 1; i <= 11; i++) {
+    document.getElementById('team1' + i + 'name').innerHTML = team1List[i-1]['name']
+    document.getElementById('team2' + i + 'name').innerHTML = team2List[i-1]['name']
+    document.getElementById('team1' + i + 'role').innerHTML = team1List[i-1]['role']
+    document.getElementById('team2' + i + 'role').innerHTML = team2List[i-1]['role']
+  }
+
+  // show results and hide loading
+  loadingView.style.display = 'none'
+  resultView.style.display = 'block'
+}
+
+pulesRates=[]
+async function getDataFromAPI(url,pageNum,diagnosisName,doctorId) {
+  const response = await fetch(`${url}?page=${pageNum}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  return response.json().then(x=>{
+    for(var record in x.data)
+    {
+      var r = x.data[record]
+       var n1 = r.vitals.bloodPressureDiastole
+       console.log(r.diagnosis.name,r.doctor.id)
+       if(r.diagnosis.name == diagnosisName && r.doctor.id == doctorId)
+       {
+        pulesRates.push(r.vitals.pulse)
+       }
+    }
+  })
+}
+
+async function healthCheckup(diagnosisName, doctorId) {
+  const apiUrl = 'https://jsonmock.hackerrank.com/api/medical_records'
+  const list=[]
+  for(var i=1;i<=10;i++)
+  {
+      list.push(getDataFromAPI(apiUrl,i,diagnosisName,doctorId))
+  }
+  await Promise.all(list).then(x=>{
+    var sum=0
+    for(var a in pulesRates)
+    sum+=pulesRates[a]
+    return Math.floor(sum/pulesRates.length)
+  })
+}
+
+healthCheckup("Pleurisy",3)
